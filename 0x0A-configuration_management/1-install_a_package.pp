cfa4a0dbd3e475681b6e9d2440e4c6a0d77ce627
Boules Hisham <bolespolo4@gmail.com>
2024-07-03 16:08:20 -0700

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,18 @@
+# install puppet-lint -v 2.5.0
+
+package { `python3.8`:
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { `Flask`:
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { `Werkzeug`:
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask']
+}
+
