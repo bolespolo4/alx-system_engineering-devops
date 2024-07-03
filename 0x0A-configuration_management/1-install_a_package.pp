bcac78e9b95b892e70a4e27c775c54eb45a04234
Boules Hisham <bolespolo4@gmail.com>
2024-07-03 16:14:02 -0700

    Install a package---

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,6 @@
-# install_flask.pp
+# install puppet-lint -v 2.5.0
 
-# Ensure python3-pip is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-# Optional: Create a symbolic link for 'flask' command if needed
-file { '/usr/local/bin/flask':
-  ensure  => link,
-  target  => '/usr/bin/flask',
-  require => Package['Flask'],
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
 
